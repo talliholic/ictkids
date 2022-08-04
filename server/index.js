b540f98e0ffc0b61197dcbae68f4cfaa9f661f26
talliholic <talliholic.com>
2022-08-04 10:59:25 -0500

    Deploy with build

--- server/index.js
+++ server/index.js
@@ -7,8 +7,15 @@ const port = process.env.PORT || 5000
 app.use(cors())
 
 app.use(express.static("build"))
-app.get("*", (req, res) => {
+
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../build", "index.html"))
+})
+app.get("/video-activit*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"))
+})
+app.get("/test", (req, res) => {
+  res.json({ test: "this is a test" })
 })
 
 app.use(express.json())
